4a9646c8d87543a3995e8a89e2d5a77cbf5d4114
Viki Flendiansyah <vfsyah@gmail.com>
2024-04-16 23:51:17 +0700

    update table products

--- app/libs/placeholder-data.js
+++ app/libs/placeholder-data.js
@@ -5,8 +5,43 @@ const users = [
     email: "syafiqfajrianemha@gmail.com",
     password: "12345678",
   },
+  {
+    id: "410544b2-4001-4271-9855-fec4b6a6442b",
+    name: "Viki Flendiansyah",
+    email: "vfsyah@gmail.com",
+    password: "12345678",
+  },
+];
+
+const products = [
+  {
+    id: 1,
+    stall: "Pentol Pak Bambang",
+    nomor: "6281225661530",
+    makanan: [
+      {
+        id: Math.random() * 100 + 1,
+        name: "Pentol Mercon",
+        price: 10000,
+        image: "/img/pentol.png",
+      },
+      {
+        id: Math.random() * 100 + 1,
+        name: "Pentol Isi Telur",
+        price: 13000,
+        image: "/img/pentolisi.jpg",
+      },
+      {
+        id: Math.random() * 100 + 1,
+        name: "Pentol Kuah",
+        price: 5000,
+        image: "/img/pentolbiasa.jpg",
+      },
+    ],
+  },
 ];
 
 module.exports = {
   users,
+  products,
 };
--- scripts/seed.js
+++ scripts/seed.js
@@ -1,5 +1,6 @@
 const { db } = require("@vercel/postgres");
 const { users } = require("../app/libs/placeholder-data.js");
+const { products } = require("../app/libs/placeholder-data.js");
 const bcrypt = require("bcrypt");
 
 async function seedUsers(client) {
@@ -37,6 +38,53 @@ async function seedUsers(client) {
     };
   } catch (error) {
     console.error("Error seeding users:", error);
+    throw error;
+  }
+}
+
+async function seedProducts(client) {
+  try {
+    // Create the "products" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS products (
+        id SERIAL PRIMARY KEY,
+        stall VARCHAR(255) NOT NULL,
+        nomor VARCHAR(255) NOT NULL,
+        makanan JSONB NOT NULL
+      );
+    `;
+
+    console.log(`Created "products" table`);
+
+    // Insert data into the "products" table
+    const insertedProducts = await Promise.all(
+      products.map(async (product) => {
+        const formattedMakanan = product.makanan.map((food) => {
+          return {
+            id: food.id,
+            name: food.name,
+            price: food.price,
+            image: food.image,
+          };
+        });
+        return client.sql`
+        INSERT INTO products (stall, nomor, makanan)
+        VALUES (${product.stall}, ${product.nomor}, ${JSON.stringify(
+          formattedMakanan
+        )})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      })
+    );
+
+    console.log(`Seeded ${insertedProducts.length} products`);
+
+    return {
+      createTable,
+      products: insertedProducts,
+    };
+  } catch (error) {
+    console.error("Error seeding products:", error);
     throw error;
   }
 }
