3ce80bc1ff76da306eaa85b6aaa7afd3142ba6ba
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2024-04-16 20:32:57 +0700

    configuration database postgres vercel

---
+++ app/libs/placeholder-data.js
@@ -0,0 +1,12 @@
+const users = [
+  {
+    id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    name: "Syafiq Fajrian Emha",
+    email: "syafiqfajrianemha@gmail.com",
+    password: "12345678",
+  },
+];
+
+module.exports = {
+  users,
+};
